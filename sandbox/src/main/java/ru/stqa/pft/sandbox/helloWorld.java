7ab25d54d8e609de23e1f77d45d092ecbc69c0bb
DmitryBorodich <59160369+DmitryBorodich@users.noreply.github.com>
2019-12-23 18:57:49 +0500

    Добавление классов и методов этих классов

--- sandbox/src/main/java/ru/stqa/pft/sandbox/helloWorld.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/helloWorld.java
@@ -8,12 +8,11 @@ public class HelloWorld
         hello("user");
         hello("Dmitry");
 
-        double l = 5;
-        System.out.println("Площадь квадрата со стороной " + l + " = " + area(l));
+        Square s = new Square(5);
+        System.out.println("Площадь квадрата со стороной " + s.l + " = " + s.area());
 
-        double a = 4;
-        double b = 6;
-        System.out.println("Площадь прямоугольника со стороной " + a + " и " + b + " = " + area(a, b));
+        Rectangle r = new Rectangle(4,6);
+        System.out.println("Площадь прямоугольника со стороной " + r.a + " и " + r.b + " = " + r.area());
     }
 
     public static void hello(String somebody)
@@ -21,14 +20,9 @@ public class HelloWorld
         System.out.println("Hello, " + somebody + "!");
     }
 
-    public static double area(double len)
-    {
-        return len * len;
 
-    }
-
-    public static double area(double a, double b)
+    public static double area(Rectangle r)
     {
-        return a * b;
+        return r.a * r.b;
     }
 }
