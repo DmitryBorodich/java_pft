86a5ddaba8cda3ab241f53e2389baaa1792d59e5
DmitryBorodich <59160369+DmitryBorodich@users.noreply.github.com>
2019-12-23 15:12:37 +0500

    скорректированны нахвания функции в соответствии с обучением

--- sandbox/src/main/java/ru/stqa/pft/sandbox/helloWorld.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/helloWorld.java
@@ -1,9 +1,8 @@
 package ru.stqa.pft.sandbox;
 
-public class helloWorld
+public class HelloWorld
 {
-	public static void main(String[] args)
-	{
-		System.out.println("Hello, world!");
-	}
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+    }
 }
