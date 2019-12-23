41ee534681e198d6ac16edd9406f366d3286e79e
DmitryBorodich <59160369+DmitryBorodich@users.noreply.github.com>
2019-12-23 19:45:18 +0500

    Создание класса Point и запускаемого класса для Задания №2

---
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/HomeWork.java
@@ -0,0 +1,19 @@
+package ru.stqa.pft.sandbox;
+
+public class HomeWork
+{
+    public static void main(String[] args)
+    {
+        Point p1 = new Point(4,6);
+        Point p2 = new Point(7,10);
+        System.out.println("Длинна между двумя двумерными точками с координатами (" + p1.a + "," + p1.b + ") и (" + p2.a + "," + p2.b + ") = " + distance1(p1,p2));
+
+        System.out.println("Длинна между двумя двумерными точками с координатами (" + p1.a + "," + p1.b + ") и (" + p2.a + "," + p2.b + ") = " + p1.distance(p1,p2));
+    }
+
+    public static double distance1(Point p1, Point p2)
+    {
+        return Math.sqrt((p1.a-p2.a)*(p1.a-p2.a) + (p1.b-p2.b)*(p1.b-p2.b));
+    }
+}
+
