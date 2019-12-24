c3b4449257c44cfa684b3ce1388fb1a1b5592b26
DmitryBorodich <59160369+DmitryBorodich@users.noreply.github.com>
2019-12-24 21:27:35 +0500

    Исправлены тесты для Point после изменений функции distance

--- sandbox/src/test/java/ru/stqa/pft/sandbox/PointTests.java
+++ sandbox/src/test/java/ru/stqa/pft/sandbox/PointTests.java
@@ -10,15 +10,15 @@ public class PointTests
     {
         Point p1 = new Point(1, 1);
         Point p2 = new Point(4, 5);
-        Assert.assertEquals(p1.distance(p1,p2), 5);
+        Assert.assertEquals(p1.distance(p2), 5);
 
         Point p3 = new Point(-1, -1);
         Point p4 = new Point(-4, -5);
-        Assert.assertEquals(p4.distance(p4,p3), 5);
+        Assert.assertEquals(p4.distance(p3), 5);
 
         Point p5 = new Point(-1, -1);
         Point p6 = new Point(-4, -5);
-        Assert.assertEquals(p4.distance(p4,p3), 6);
+        Assert.assertEquals(p4.distance(p4), 6);
 
     }
 }
