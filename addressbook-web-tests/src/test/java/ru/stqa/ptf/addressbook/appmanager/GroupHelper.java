54c7ba3543915eaf7df622e4cec1c8c1302c79b8
DmitryBorodich <59160369+DmitryBorodich@users.noreply.github.com>
2020-01-26 19:04:08 +0500

    Проверка количества созданных тестов для групп

--- addressbook-web-tests/src/test/java/ru/stqa/ptf/addressbook/appmanager/GroupHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/ptf/addressbook/appmanager/GroupHelper.java
@@ -56,4 +56,8 @@ public class GroupHelper  extends HelperBase  {
 
         return isElenentPresent(By.name("selected[]"));
     }
+
+    public int getGroupCount() {
+        return wd.findElements(By.name("selected[]")).size();
+    }
 }
