0214b9239636e3527f3eeaee3dd97d3e66d174a1
DmitryBorodich <59160369+DmitryBorodich@users.noreply.github.com>
2020-02-03 12:57:17 +0500

    Изменен способ запуска браузера

--- addressbook-web-tests/src/test/java/ru/stqa/ptf/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/stqa/ptf/addressbook/tests/TestBase.java
@@ -1,26 +1,26 @@
 package ru.stqa.ptf.addressbook.tests;
 
 
-import org.openqa.selenium.*;
+import org.openqa.selenium.Alert;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.remote.BrowserType;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
 import ru.stqa.ptf.addressbook.appmanager.ApplicationManager;
 
 
 public class TestBase {
 
-    protected final ApplicationManager app = new ApplicationManager(BrowserType.CHROME);
+    protected static final ApplicationManager app = new ApplicationManager(BrowserType.CHROME);
     protected WebDriver wd;
     protected boolean acceptNextAlert = true;
 
-    @BeforeMethod(alwaysRun = true)
+    @BeforeSuite(alwaysRun = true)
     public void setUp() throws Exception {
         app.init();
     }
 
-    @AfterMethod(alwaysRun = true)
+    @AfterSuite(alwaysRun = true)
     public void tearDown() throws Exception {
         app.stop();
     }
