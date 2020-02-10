b96687545d857f42bf35d6158e9d7540138afc92
DmitryBorodich <59160369+DmitryBorodich@users.noreply.github.com>
2020-02-10 21:23:47 +0500

    Генератор тестовых данных для телефонов

--- addressbook-web-tests/src/test/java/ru/stqa/ptf/addressbook/model/ContactData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/ptf/addressbook/model/ContactData.java
@@ -1,12 +1,13 @@
 package ru.stqa.ptf.addressbook.model;
 
 import com.thoughtworks.xstream.annotations.XStreamAlias;
+import com.thoughtworks.xstream.annotations.XStreamOmitField;
 
 import java.io.File;
 import java.util.Objects;
 @XStreamAlias("contact")
 public class ContactData {
-
+    @XStreamOmitField
     private int id = Integer.MAX_VALUE;
     private String firstname;
     private String lastname;
