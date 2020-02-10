43e23139aedfad4780fbc2e8f9acfed934ad8705
DmitryBorodich <59160369+DmitryBorodich@users.noreply.github.com>
2020-02-10 21:48:36 +0500

    Параметризован тест для создания контактов

--- addressbook-web-tests/src/test/java/ru/stqa/ptf/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/ptf/addressbook/tests/ContactCreationTests.java
@@ -1,10 +1,18 @@
 package ru.stqa.ptf.addressbook.tests;
 
+import com.thoughtworks.xstream.XStream;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import ru.stqa.ptf.addressbook.model.ContactData;
 import ru.stqa.ptf.addressbook.model.Contacts;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -12,14 +20,25 @@ import static org.testng.Assert.assertEquals;
 
 public class ContactCreationTests extends TestBase  {
 
-  @Test
-  public void testContactCreation() {
+  @DataProvider
+  public Iterator<Object[]> validContacts() throws IOException {
+    BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/contacts.xml")));
+    String xml = "";
+    String line = reader.readLine();
+    while (line != null) {
+      xml += line;
+      line = reader.readLine();
+    }
+    XStream xstream = new XStream();
+    xstream.processAnnotations(ContactData.class);
+    List<ContactData> groups = (List<ContactData>) xstream.fromXML(xml);
+    return groups.stream().map((g) -> new Object[]{g}).collect(Collectors.toList()).iterator();
+  }
+
+  @Test(dataProvider = "validContacts")
+  public void testContactCreation(ContactData contact) {
     app.сontact().gotoHomePage();
     Contacts before = app.сontact().all();
-    File photo = new File("src/test/resources/stru.png");
-    ContactData contact = new ContactData().withFirstname("jon1").withLastname("smit1").withEmail("2@2.2").withAddress("test")
-            .withHomePhone("+79999999991").withMobile("+79999999992").withWorkPhone("+79999999993")
-            .withPhoto(photo);
     app.сontact().create(contact);
     Contacts after = app.сontact().all();
 
